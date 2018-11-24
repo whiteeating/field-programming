3b6eed4444e95eabbcd8d3dc6d086d1a0118fbf3
LU-K-Brant <42348398+LU-K-Brant@users.noreply.github.com>
2018-11-24 21:52:49 +0800

    Add files via upload

--- src/temp.py
+++ src/temp.py
@@ -2,8 +2,7 @@ import tkinter
 import random
 import threading
 import time
-from randomdraw import *
-from txt_solve import *
+from randomdraw import draw_winner
 from PIL import Image, ImageTk
 from tkinter import ttk
 #初始化窗口
@@ -14,6 +13,7 @@ root.resizable(False,False)
 root.flag = True
 root.start=False
 k='22'
+count = 0
 #背景
 
 canvas = tkinter.Canvas(root,
@@ -29,9 +29,14 @@ canvas.pack()
 #关键词 时间范围
 
 label1 = tkinter.Label(root, text="输入关键词：",bg='#920a0c',fg='#FFFFFF',font = ("宋体", 20,"normal"))
-label1.place(x=768, y=503)
+label1.place(x=768, y=480)
+
+label3 = tkinter.Label(root, text="输入抽奖个数：",bg='#920a0c',fg='#FFFFFF',font = ("宋体", 20,"normal"))
+label3.place(x=768, y=512)
+
 label2 = tkinter.Label(root, text="请选择时间范围：",bg='#920a0c',fg='#FFFFFF',font = ("宋体", 20,"normal"))
 label2.place(x=768, y=542)
+
 labelmonth1 = tkinter.Label(root, text="月",bg='#920a0c',fg='#FFFFFF',font = ("宋体", 20,"normal"))
 labelmonth1.place(x=820, y=581)
 labelday1 = tkinter.Label(root, text="日-",bg='#920a0c',fg='#FFFFFF',font = ("宋体", 20,"normal"))
@@ -60,15 +65,77 @@ five.place(x=819,y=373,width=166,height=41)
 six = tkinter.Label(root,text='',bg='#920a0c',font = ("宋体", 25,"normal"),fg='#F4D267')
 six.place(x=819,y=414,width=166,height=41)
 
+
 #滚动
 def switch():
     root.flag=True
-    win=[]
-    print(k)
-    win=draw_winner(k)
+    #win=[]
+    #print(k)
+    win=draw_winner(k,count)
     print(win)
-    while root.flag:
-        a=random.sample(win,6)
+    #a=random.sample(win,count)
+    if count == 1:
+        while root.flag:
+            win =random.sample(win,count)
+            first['text'] = win[0]
+            second['text']=''
+            third['text']=''
+            four['text']=''
+            five['text']=''
+            six['text']=''
+            time.sleep(0.03)
+    if count == 2:
+        while root.flag:
+            win =random.sample(win,count)
+            first['text']= win[0]
+            second['text']=win[1]
+            third['text']=''
+            four['text']=''
+            five['text']=''
+            six['text']=''
+            time.sleep(0.03)
+    if count == 3:
+        while root.flag:
+            win =random.sample(win,count)
+            first['text']= win[0]
+            second['text']=win[1]
+            third['text']=win[2]
+            four['text']=''
+            five['text']=''
+            six['text']=''
+            time.sleep(0.03)
+    if count == 4:
+        while root.flag:
+            win =random.sample(win,count)
+            first['text']= win[0]
+            second['text']=win[1]
+            third['text']=win[2]
+            four['text']=win[3]
+            five['text']=''
+            six['text']=''
+            time.sleep(0.03)
+    if count == 5:
+        while root.flag:
+            win =random.sample(win,count)
+            first['text']= win[0]
+            second['text']=win[1]
+            third['text']=win[2]
+            four['text']=win[3]
+            five['text']=win[4]
+            six['text']=''
+            time.sleep(0.03)
+    if count == 6:
+        while root.flag:
+            win =random.sample(win,count)
+            first['text']= win[0]
+            second['text']=win[1]
+            third['text']=win[2]
+            four['text']=win[3]
+            five['text']=win[4]
+            six['text']=win[5]
+            time.sleep(0.03)
+        """
+        a=random.sample(win,count)
         first['text']=a[0]
         second['text']=a[1]
         third['text']=a[2]
@@ -76,6 +143,8 @@ def switch():
         five['text']=a[4]
         six['text']=a[5]
         time.sleep(0.03)
+        """
+       
 
 
 #开始按钮
@@ -104,14 +173,24 @@ typeEntered.focus()
 def rtnkey(event=None):
     #print(e.get())
     global k
-    k=e.get()
+    k = e.get()
     #return e.get()
 
 e = tkinter.StringVar()
 entry = ttk.Entry(root, validate='key', textvariable=e, width=30)
-entry.place(x=920, y=505, height=30)
+entry.place(x=920, y=480, height=30)
 entry.focus()  # 当程序运行时,光标默认会出现在该文本框中
-entry.bind('<Return>', rtnkey)
+entry.bind('<Leave>',rtnkey)
+
+def numkey(event = None):
+    global count 
+    count = int(f.get())
+
+f = tkinter.StringVar()
+entry = ttk.Entry(root, validate='key', textvariable=f, width=10)
+entry.place(x=950, y=515, height=30)
+entry.focus()  # 当程序运行时,光标默认会出现在该文本框中
+entry.bind('<Leave>', numkey)
 
 #下拉框
 numbermonth1 = tkinter.StringVar()
--- src/txt_solve.py
+++ src/txt_solve.py
@@ -11,26 +11,22 @@ Created on Sat Nov 17 15:54:13 2018
 
 @author: 14194
 """
-
-import os
 import sys
 import re
 import numpy as np
 import pandas as pd
 from datetime import datetime
-match1 = '系统消息'
-def matchh(value):
-    match2=value
-    return match2
 match2 = '2'
-l = [0]
-person =[]
-able_person = []
-file = open('record.txt','r',encoding='utf-8')
+
+"""def matchh(value):
+    match2=value
+    return match2 """
 def wipe_empty():
+    file = open('record.txt','r',encoding='utf-8')
     fnew = open('result.txt','w',encoding='utf-8')
     lines = file.readlines()
     l = [x for x in lines]
+    match1 = '系统消息'
     for n in range(len(l)):
         data = l[n].strip()
         if len(data)!= 0:
@@ -41,28 +37,33 @@ def wipe_empty():
                 l[n] = l[n+1] = ''
                 fnew.write(l[n])
                 fnew.write(l[n+1])
-    fnew.close()
+                
 def select_1(value):
+    wipe_empty()
     fnew = open('result.txt','r+',encoding='utf-8')
+
     filenew = open('final_result.txt','w',encoding='utf-8')
     f = fnew.readlines()
+    
+    # print(f)
     l = [x for x in f]
-    match2=value
+    match2 = value
+    
     print(match2+'ok1')
+    
     for n in range(len(l)):
         if match2 in l[n]:
             filenew.write(l[n-1])
             l[n] = match2+'\n'
             filenew.write(l[n])
-    fnew.close()
-    pass
 def select_QQ(value):
-    wipe_empty()
     select_1(value)
     f = open('final_result.txt','r',encoding='utf-8')
     qqNum = open('QQ_NUM.txt','w')
+    person = []
+    able_person = []
+    s = set([])
     b = 1
-    s = set(l)
     for line in f.readlines():
         rule = re.compile(r'[1-9][0-9]{5,9}')
         r = re.compile(r'[1-9](0){6,10}')
@@ -77,17 +78,14 @@ def select_QQ(value):
             #print(m.group())
             person.append(a)
         s = set(person)
+    #print(s)
     for x in s:
-        if x!=b:
+        if x!= b:
             able_person.append(x)
             x = str(x)
             qqNum.write(x+'\n')
-    return able_person
-if __name__ == "__main__":
-    wipe_empty()
-    select_1(value)
-    select_QQ(value)
-    
+    return len(able_person),able_person
+
     
     
     
